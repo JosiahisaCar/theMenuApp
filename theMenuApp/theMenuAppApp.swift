ffc02d08000fadef6de6af02451241f383565658
Josiah <josiah.carver@icloud.com>
2025-11-28 01:32:26 -0500

    Initial Commit

---
+++ theMenuApp/theMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  theMenuAppApp.swift
+//  theMenuApp
+//
+//  Created by Josiah Carver on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct theMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
