c7e58bb543a967dab8eda07074515a4ebaa8a665
Josiah <josiah.carver@icloud.com>
2025-11-28 01:55:04 -0500

    finished chapter 3 structures

---
+++ theMenuApp/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  theMenuApp
+//
+//  Created by Josiah Carver on 11/28/25.
+//
+
+import Foundation
+
+struct MenuItem{
+    var name: String
+    var price: String
+    var imageName: String
+}
